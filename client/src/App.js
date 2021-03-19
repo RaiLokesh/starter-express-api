e0f6048fb98f6ad62575912b3f9f0b4880e3d806
Lokesh Rai <lokeshrai2019@iiitkottayam.ac.in>
2021-03-19 23:59:53 +0530

    next-commit

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import templogo from 'D:/Project Related (PERSONAL)/sinecure/server/client/src/templogo.png';
 import NavBar from './components/Navbar'
 
+//font used: Questrial - 400
 
 function App() {
   
@@ -9,13 +10,16 @@ function App() {
     <div>
       <div className = "split left">
       <div className="centered">
-        <img src={templogo}></img>
+        <img src={templogo} className = "centered"></img>
         
       </div>
       </div>
       <div className = "split right">
         <div className="container">
-          <NavBar/>
+          <div><br></br><br></br></div>
+          <div className="centerednav">
+            <NavBar/>
+          </div>
         </div>
       </div>
     </div>
