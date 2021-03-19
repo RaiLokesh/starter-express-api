4887f80df170779b8306cb88d1e68b11da120e8c
Lokesh Rai <lokeshrai2019@iiitkottayam.ac.in>
2021-03-19 18:54:39 +0530

    next-commit

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import templogo from 'D:/Project Related (PERSONAL)/sinecure/server/client/src/templogo.png';
+import NavBar from './components/Navbar'
+
 
 function App() {
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className = "split left">
+      <div className="centered">
+        <img src={templogo}></img>
+        
+      </div>
+      </div>
+      <div className = "split right">
+        <div className="container">
+          <NavBar/>
+        </div>
+      </div>
     </div>
   );
 }
